72a95d1e0eaef674c19d63495d3097ffc49caa38
PrinsFrank <25006490+PrinsFrank@users.noreply.github.com>
2026-08-29 19:18:47 +0200

    Add and configure phpcsfixer

---
+++ .php-cs-fixer.php
@@ -0,0 +1,28 @@
+<?php
+
+return (new PhpCsFixer\Config())->setRules(
+    [
+        '@PER-CS3.0'                   => true,
+        'declare_strict_types'         => true,
+        'strict_comparison'            => true,
+        'blank_line_after_opening_tag' => false,
+        'no_extra_blank_lines'         => true,
+        'class_attributes_separation'  => ['elements' => ['const' => 'only_if_meta', 'method' => 'one', 'property' => 'only_if_meta', 'trait_import' => 'only_if_meta', 'case' => 'only_if_meta']],
+        'array_syntax'                 => ['syntax' => 'short'],
+        'binary_operator_spaces'       => ['default' => 'single_space'],
+        'no_unused_imports'            => true,
+        'array_indentation'            => true,
+        'no_alias_functions'           => true,
+        'phpdoc_line_span'             => ['case' => 'single', 'class' => 'single', 'const' => 'single', 'function' => 'single', 'method' => 'single', 'other' => 'single', 'property' => 'single', 'trait_import' => 'single'],
+        'phpdoc_order'                 => ['order' => ['deprecated', 'internal', 'standard', 'source', 'template', 'implements', 'extends', 'covers', 'coversDefaultClass', 'param', 'throws', 'return']],
+        'phpdoc_param_order'           => true,
+        'phpdoc_separation'            => ['groups' => [['deprecated', 'internal'], ['standard', 'source'], ['covers', 'coversDefaultClass'], ['template', 'implements', 'extends', 'param', 'throws', 'return']]],
+        'phpdoc_trim'                  => true,
+        'concat_space'                 => ['spacing' => 'one'],
+        'braces_position'              => ['classes_opening_brace' => 'same_line', 'control_structures_opening_brace' => 'same_line', 'functions_opening_brace' => 'same_line'],
+    ]
+)->setFinder(
+    PhpCsFixer\Finder::create()
+        ->exclude('vendor')
+        ->in(__DIR__)
+)->setCacheFile('./cache/.php-cs-fixer.cache');
